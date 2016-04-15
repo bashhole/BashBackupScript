63b1263b69e75610278c6b2afc3d3ae9cc1c6d47
bashhole <desna.gold@gmail.com>
2016-04-15 12:11:00 +0400

    Update file-dump.sh

--- file-dump.sh
+++ file-dump.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# (1) set up all the mysqldump variables
+# (1) set up all the server connect variables
 FILE=full.files.`date +"%Y%m%d"`
 HOSTNAME=`curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'`.`hostname`
 LOGINSSH=youlogin
@@ -11,10 +11,10 @@ unalias rm     2> /dev/null
 rm ${FILE}     2> /dev/null
 rm ${FILE}.gz  2> /dev/null
 
-# use this command for a database server on localhost. add other options if need be.
+# use this command for a archive server on localhost. add other options if need be.
 cd /backup/
 rsync -avzh /home/ ${FILE}
-# (4) gzip the mysql database dump file
+# (4) gzip the file archive dump file
 tar -czvf $FILE.tar.gz $FILE
 rm -rf $FILE
 # (5) show the user the result
